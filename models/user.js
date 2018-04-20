bbb2b2890a7a9d0ae6d9c2b4faac305e96c79e51
ggolish <ggolish@sycamores.indstate.edu>
2018-04-20 12:25:54 -0400

    Created a model for users

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = module.exports = mongoose.model("User", userSchema);
+
+module.exports.registerUser = function(username, email, password, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    if(err) throw err;
+    bcrypt.hash(password, salt, (err, hash) => {
+      if(err) throw err;
+      let user = new User({
+        username: username,
+        email: email,
+        password: hash
+      });
+      user.save(callback);
+    });
+  });
+}
+
+module.exports.validatePassword = function(id, password, callback) {
+  User.findOne({_id: id}, (err, user) => {
+    if(err) throw err;
+    if(!user) {
+      callback(null, false);
+      return;
+    }
+    bcrypt.compare(password, user.password, (err, res) => {
+      if(err) throw err;
+      callback(user, res);
+    });
+  });
+}
