6a1bfb7f24ea541bec224593aae5c46f15ec5f67
ggolish <ggolish@sycamores.indstate.edu>
2018-04-20 18:05:51 -0400

    Implemented login procedure.

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -10,16 +12,22 @@ export class LoginComponent implements OnInit {
   username: string;
   password: string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    console.log({
-      username: this.username,
-      password: this.password
-    });
+    this.authService.loginUser(this.username, this.password).subscribe(
+      (data) => {
+        if(data.success) {
+          this.authService.storeUserJWT(data.user, data.token);
+          this.router.navigate(["dashboard"]);
+        } else {
+          alert(data.msg);
+        }
+      }
+    );
   }
 
 }
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable, isDevMode } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import { tokenNotExpired } from "angular2-jwt";
+import { User } from "../interfaces/user.interface";
 import "rxjs/add/operator/map"
 
 @Injectable()
 export class AuthService {
 
   url: string;
+  token: string;
+  user: User;
 
   constructor(private http: Http) {
     if(isDevMode()) {
@@ -27,8 +31,37 @@ export class AuthService {
     return this.http.post(endpoint, content, {headers: headers}).map(res => res.json())
   }
 
-  loginUser() {
+  loginUser(username: string, password: string) {
+    let endpoint = this.url + "users/login";
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    let content = JSON.stringify({
+      username: username,
+      password: password
+    });
+    return this.http.post(endpoint, content, {headers: headers}).map(res => res.json())
+  }
 
+  logoutUser() {
+    this.user = null;
+    this.token = null;
+    localStorage.clear();
+  }
+
+  storeUserJWT(user: User, token: string) {
+    localStorage.setItem("id_token", token);
+    localStorage.setItem("user", JSON.stringify(user));
+    this.token = token;
+    this.user = user;
+  }
+
+  loadUserJWT() {
+    this.token = localStorage.getItem("id_token");
+    this.user = JSON.parse(localStorage.getItem("user"));
+  }
+
+  loggedIn() {
+    return tokenNotExpired();
   }
 
 }
