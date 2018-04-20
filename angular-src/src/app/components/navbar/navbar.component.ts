39917025a15cad2507ba67cd018df4e3de75752a
ggolish <ggolish@sycamores.indstate.edu>
2018-04-20 16:02:41 -0400

    Added collapsable navbar.

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  show: boolean = false;
+
   constructor() { }
+
+  toggleCollapse() {
+    this.show = !this.show;
+  }
 
   ngOnInit() {
   }
